4bc80ee63092deb66dd25a251b8f2444ee2c3a24
Abhilash Mohapatra <128177914+Abhilash020@users.noreply.github.com>
2024-08-25 12:09:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app
+  .route("/bfhl")
+  .get((req, res) => {
+    res.status(200).json({ operation_code: 1 });
+  })
+  .post((req, res) => {
+
+    const data = req.body.data || [];
+    const numbers = [];
+    const alphabets = [];
+    let highest_lowercase_alphabet = "";
+
+  for (const item of data) {
+    if (!isNaN(item)) {
+      numbers.push(item);
+    } else if (item.length === 1 && isNaN(item)) {
+      if (item >= 'a' && item <= 'z') {
+        alphabets.push(item);
+        if (!highest_lowercase_alphabet || item > highest_lowercase_alphabet) {
+          highest_lowercase_alphabet = item;
+        }
+      }
+    }
+  }
+
+    res.json({
+      is_success: true,
+      user_id: "abhilash_mohapatra_05022004",
+      email: "abhilash.mohapatra2021@vitbhopal.ac.in",
+      roll_number: "21BEC10436",
+      numbers: numbers,
+      alphabets: alphabets,
+      highest_lowercase_alphabet: highest_lowercase_alphabet ? [highest_lowercase_alphabet] : [],
+    });
+  });
+
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
